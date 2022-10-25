935a532df56f428b0896918e4dac2807ec3170ea
seokgeun <trr456@naver.com>
2022-10-25 21:39:03 +0900

    클래스명 자동추가 테스트

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
+import styled from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className={styled.title}>내가 돌아왔다!!!</h1>
+      <Button text={"계속"}/>
     </div>
   );
 }

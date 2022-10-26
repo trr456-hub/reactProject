34980210f6e2d03acd9d2244e3e7853646aa1aec
gorhf9397 <trr456@naver.com>
2022-10-26 22:16:15 +0900

    카운트 증가 및 onClick-react사용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Button from "./Button";
-import styled from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue(prev => prev + 1);
+  console.log("호출");
   return (
     <div>
-      <h1 className={styled.title}>내가 돌아왔다!!!</h1>
-      <Button text={"계속"}/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>클릭</button>
     </div>
   );
 }

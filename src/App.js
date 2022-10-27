b128ad6b93e80f1d835412ef7e39e82a67000f81
gorhf9397 <trr456@naver.com>
2022-10-27 15:30:21 +0900

    검색창활용,useEffect응용

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue(prev => prev + 1);
-  console.log("나는 살아있다");
+  const onChange = (event) => setKeyword(event.target.value);
   useEffect(() => {
-    console.log("API를 불러옵니다.")
+    console.log("나는 한번만 실행돼요");
   }, []);
+  useEffect(() => {
+    console.log("나는 키워드가 변화할 때 실행되요.")
+  }, [keyword]);
+  useEffect(() => {
+    console.log("나는 카운터가 변화할 때 실행되요.")
+  }, [counter]);
+  useEffect(() => {
+    console.log("키워드가 변화합니다. & 카운터가 변화합니다.")
+  }, [keyword,counter]);
   return (
     <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder="검색" />
       <h1>{counter}</h1>
       <button onClick={onClick}>클릭</button>
     </div>

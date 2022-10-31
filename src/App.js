997ec08cb114afaf54e7b5309659703663e03b9a
gorhf9397 <trr456@naver.com>
2022-10-31 21:02:11 +0900

    영화api활용,js분리하여content 접속페이지 생성

--- src/App.js
+++ src/App.js
@@ -1,62 +1,7 @@
-import { useEffect, useState } from "react";
+
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [cost, setCost] = useState(1);
-  const [need, setNeed] = useState(1);
-
-  const numOne = document.getElementsByTagName("option")[0];
-  console.log("option의 첫번째 index값 : ",numOne);
-
-  const onChange = (event) => {
-    const nnn = event.target.value;
-    setCost(nnn);
-    console.log("온체인지 : ",nnn);
-  };
-  const handleInput = (event) => {
-    setNeed(event.target.value);
-    console.log("핸들인풋 : ",event.target.value);
-  };
-  //console.log(need);
-  //console.log(cost);
-  //console.log(handleInput);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []); //빈 배열을 두면 한번만 실행
-  return (
-    <div>
-      <h1>코인시세({coins.length})</h1>
-      {loading ? (
-        <strong>...로딩중</strong>
-      ) : (
-        <select onChange={onChange}>
-          {coins.map((coin) => (
-            <option
-              key={coin.id}
-              value={coin.quotes.USD.price}
-            >
-              {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
-      )}
-      <div>
-        <input
-          onChange={handleInput}
-          value={need}
-          type="number"
-          placeholder="보유한 현금"
-        />
-        <h2>변환된 가격 : {need / cost}</h2>
-      </div>
-    </div>
-  );
+  return null;
 }
 
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import Movie from "../components/Movie";
+
+function Home() {
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const getMovies = async () => {
+        const json = await (
+            await fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=8.0&sort_by=year')
+        ).json();
+        setMovies(json.data.movies)
+        setLoading(false);
+    };
+    useEffect(() => {
+        getMovies();
+    }, []);
+    return (
+        <div>
+            {/* key 는 unique 값으로 들어가야 함으로 api에있는 고유한id 값을 넣어준다. */}
+            {loading ? (<h1>로딩중...</h1>
+            ) : (
+                <div>
+                    {
+                        movies.map((movie) =>
+                            <Movie
+                                key={movie.id}
+                                coverImg={movie.medium_cover_image}
+                                title={movie.title}
+                                summary={movie.summary}
+                                genres={movie.genres}
+                            />
+                        )}
+                </div>
+            )}
+        </div>
+    );
+}
+export default Home;

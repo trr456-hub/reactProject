084f11390f097b01b324916b8931538aebba4a40
gorhf9397 <trr456@naver.com>
2022-11-01 13:47:34 +0900

    버전차이때문에 Swith => Routes 변경 <Link> 사용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// react-router-dom 의 버전이 6 버전 으로 올라가며 Swich => Routes 명령어로 변경됨.
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  return null;
+  return <Router>
+    <Routes>
+      <Route path="/movie" element={<Detail />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </Router>;
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,14 @@
 import PropTypes from "prop-types";
-
+import { Link } from "react-router-dom";
 function Movie({ coverImg, title, summary, genres }) {
     return <div>
         <img src={coverImg} alt={title} />
-        <h2>{title}</h2>
+        <h2>
+            <Link to="/movie">{title}</Link>
+        </h2>
+        {/* a태그를 사용해도 되지만 페이지가 전체 세로고침이된다.
+            <Link to=""> router-dom 을 사용해 페이지 이동후에도 페이지 의 새로고침을 막는다.
+        */}
         <p>{summary}</p>
         <ul>
             {genres.map((g) => <li key={g}>{g}</li>)}

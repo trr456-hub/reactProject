03c96e8f4dc6608ae54ee7fb1598f54dbd4d4475
seokgeun <trr456@naver.com>
2022-11-10 22:03:10 +0900

    들여쓰기 수정

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -2,31 +2,36 @@ import { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail() {
-    const { id } = useParams();
-    const [loading, setLoading] = useState(true);
-    const [movie, setMovie] = useState([]);
-    const getMovie = useCallback(async () => {
-        const json = await (
-            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
-        ).json();
-        //console.log(json);
-        setMovie(json.data.movie)
-        setLoading(false);
-    },[id]);
-    useEffect(() => {
-        getMovie();
-    }, [getMovie]);
-    return (
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
+  const getMovie = useCallback(async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    //console.log(json);
+    setMovie(json.data.movie);
+    setLoading(false);
+  }, [id]);
+  useEffect(() => {
+    getMovie();
+  }, [getMovie]);
+  return (
+    <div>
+      {loading ? (
+        <h1>영화 정보 불러오는중...</h1>
+      ) : (
         <div>
-            {loading ? (<h1>영화 정보 불러오는중...</h1>
-            ) : (<div>
-                <img src={movie.large_cover_image}/>
-                <h2>{movie.title} ({movie.language})</h2>
-                <hr/>
-                <p>{movie.description_full}</p>
-            </div>)}
+          <img src={movie.large_cover_image} />
+          <h2>
+            {movie.title} ({movie.language})
+          </h2>
+          <hr />
+          <p>{movie.description_full}</p>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default Detail;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,42 +3,43 @@ import Movie from "../components/Movie";
 import styles from "./Home.module.css";
 
 function Home() {
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-    const getMovies = async () => {
-        const json = await (
-            await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.0&sort_by=year`)
-        ).json();
-        setMovies(json.data.movies)
-        setLoading(false);
-    };
-    useEffect(() => {
-        getMovies();
-    }, []);
-    return (
-        <div className={styles.container}>
-            {/* key 는 unique 값으로 들어가야 함으로 api에있는 고유한id 값을 넣어준다. */}
-            {loading ? (
-                <div className={styles.loader}>
-                    <span>Loading...</span>
-                </div>
-            ) : (
-                <div className={styles.movies}>
-                    {
-                        movies.map((movie) =>
-                            <Movie
-                                key={movie.id}
-                                id={movie.id}
-                                year={movie.year}
-                                coverImg={movie.medium_cover_image}
-                                title={movie.title}
-                                summary={movie.summary}
-                                genres={movie.genres}
-                            />
-                        )}
-                </div>
-            )}
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.0&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return (
+    <div className={styles.container}>
+      {/* key 는 unique 값으로 들어가야 함으로 api에있는 고유한id 값을 넣어준다. */}
+      {loading ? (
+        <div className={styles.loader}>
+          <span>Loading...</span>
         </div>
-    );
+      ) : (
+        <div className={styles.movies}>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              year={movie.year}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 export default Home;
